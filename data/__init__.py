9425353a7821be7945f79c5b024721ada3b99304
shubhamgore2468 <shubhamgore2468@gmail.com>
2026-04-08 15:33:36 -0700

    RAY for data processing - init 2

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,16 @@
 from .dataset import VQADataset, collate_fn, prepare_vqa_samples, save_vqa_dataset
-from .ray_pipeline import create_pipeline, create_pipeline_with_features, save_to_parquet, to_torch_dataloader, run_pipeline, RayDataloader
+from .ray_pipeline import create_pipeline, create_pipeline_with_features, save_to_parquet, to_torch_dataloader, run_pipeline
 from .ray_dataloader import RayDataloader
 
-__all__ = ["VQADataset", "collate_fn", "prepare_vqa_samples", "save_vqa_dataset", "create_pipeline", "create_pipeline_with_features", "save_to_parquet", "to_torch_dataloader", "run_pipeline", "RayDataloader"]
+__all__ = [
+    "VQADataset",
+    "collate_fn",
+    "prepare_vqa_samples",
+    "save_vqa_dataset",
+    "create_pipeline",
+    "create_pipeline_with_features",
+    "save_to_parquet",
+    "to_torch_dataloader",
+    "run_pipeline",
+    "RayDataloader",
+]
